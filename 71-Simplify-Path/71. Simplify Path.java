c517e6e8fb14dcedc4764214befa99f895a6c614
HannibalCJH <hannibalcjh@gmail.com>
2016-08-06 15:09:43 -0500

    [71. Simplify Path][Accepted]committed by LeetCode Extension

---
+++ 71-Simplify-Path/71. Simplify Path.java
@@ -0,0 +1,46 @@
+public class Solution {
+    public String simplifyPath(String path) 
+    {
+        int length = path.length();
+        if(length == 0)
+            return "/";
+        
+        Stack<String> st = new Stack<String>();
+        int i = 0;
+        while(i < length)
+        {
+            char cur = path.charAt(i);
+            if(cur == '/')
+            {
+                int right = i + 1;
+                while(right < length && path.charAt(right) != '/') right++;
+                String part = path.substring(i + 1, right);
+                i = right;
+                // 连续两个斜杠"//"
+                if(part.length() == 0)
+                    continue;
+                    
+                if(part.equals(".."))
+                {
+                    if(!st.isEmpty())
+                        st.pop();
+                }
+                else if(part.equals("."))
+                    continue;
+                else
+                    st.push(part);
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty())
+        {
+            sb.insert(0, st.pop());
+            sb.insert(0, "/");
+        }
+        
+        if(sb.length() == 0)
+            return "/";
+        return sb.toString();
+    }
+}
