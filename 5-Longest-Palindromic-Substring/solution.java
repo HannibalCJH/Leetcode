41b43182ece404db2d380721f979bb0e8f81c71a
HannibalCJH <hannibalcjh@gmail.com>
2016-08-06 17:13:03 -0500

    [5. Longest Palindromic Substring][Accepted]committed by LeetCode Extension

--- 5-Longest-Palindromic-Substring/solution.java
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -1,30 +1,36 @@
 public class Solution {
     public String longestPalindrome(String s) 
     {
-        if(s.length() == 0)  
-            return "";  
-        String result = s.substring(0,1);  
-        for(int i=0;i<s.length()-1;i++)
-        {  
-            String temp = expandAroundCenter(s, i, i);  
-            if(temp.length() > result.length())  
-            	result = temp;  
-            
-            temp = expandAroundCenter(s, i, i + 1);  
-            if(temp.length() > result.length())  
-            	result = temp;  
-        }  
-        return result;     
+        if(s == null || s.length() == 0)
+            return "";
+        
+        // 初始化结果字符为第一个字符
+        String result = s.substring(0, 1);
+        for(int i = 0; i < s.length() - 1; i++)
+        {
+            // 以一个字符为中心
+            String temp = expandFromCenter(s, i, i);
+            if(temp.length() > result.length())
+                result = temp;
+            // 以两个字符为中心
+            if(s.charAt(i) == s.charAt(i + 1))
+            {
+                temp = expandFromCenter(s, i, i + 1);
+                if(temp.length() > result.length())
+                    result = temp;
+            }
+        }
+        return result;
     }
     
-    public String expandAroundCenter(String s, int left, int right)
-    {  
-        int len = s.length();  
-        while(left > -1 && right < len && s.charAt(left) == s.charAt(right))
-        {  
-            left--;  
-            right++;  
-        }  
-        return s.substring(left + 1, right);  
-    } 
+    private String expandFromCenter(String str, int left, int right)
+    {
+        // 从中心向左右两边比较
+        while(left >= 0 && right < str.length() && str.charAt(left) == str.charAt(right))
+        {
+            left--;
+            right++;
+        }
+        return str.substring(left + 1, right);
+    }
 }
