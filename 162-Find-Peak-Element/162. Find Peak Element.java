25048f03a9637f5d14eff3906aec1e2232997adf
HannibalCJH <hannibalcjh@gmail.com>
2016-08-11 21:11:37 -0500

    [162. Find Peak Element][Accepted]committed by LeetCode Extension

--- 162-Find-Peak-Element/162. Find Peak Element.java
+++ 162-Find-Peak-Element/162. Find Peak Element.java
@@ -1,19 +1,18 @@
 public class Solution {
     public int findPeakElement(int[] nums) 
-	{
-		int left = 0, right = nums.length - 1;
-		while(left < right)
-		{
-			int mid = left + (right - left) / 2;
-			// 左右边界挨着
-			if(left == mid)
-				return nums[left] < nums[right] ? right : left;
-			// 如果右边是上坡，则抛弃左边，设left等于mid
-			left = nums[mid] < nums[mid + 1] ? mid : left;
-			// 如果左边是上坡，则抛弃右边边，设right等于mid
-			right = nums[mid] > nums[mid + 1] ? mid : right;
-		}
-		
-		return 0;
+    {
+        int left = 0, right = nums.length - 1;
+        while(left < right)
+        {
+            int mid = (left + right) / 2;
+            // 左右边界相邻
+            if(left == mid)
+                return nums[left] < nums[right] ? right : left;
+            if(nums[mid] < nums[mid + 1])
+                left = mid;
+            if(nums[mid] > nums[mid + 1])
+                right = mid;
+        }
+        return 0;
     }
 }
