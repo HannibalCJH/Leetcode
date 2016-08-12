44bc8915917a0c96f33d2a3bb31914692e111369
HannibalCJH <hannibalcjh@gmail.com>
2016-08-11 20:10:32 -0500

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/91. Decode Ways.java
+++ 91-Decode-Ways/91. Decode Ways.java
@@ -3,11 +3,15 @@ public class Solution {
 	{
         if(s.length() == 0) 
         	return s.length();
+        
+        // 如果第一位是0，则无法解码
+        if(s.charAt(0) == '0')
+            return 0;
+            
         int[] dp = new int[s.length() + 1];
         // 初始化第一种解码方式
         dp[0] = 1;
-        // 如果第一位是0，则无法解码
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        dp[1] = 1;
         for(int i=2;i<=s.length();i++)
         {
             // 如果字符串的第i-1位和第i位能组成一个10到26的数字，说明我们可以在第i-2位的解码方法上继续解码
