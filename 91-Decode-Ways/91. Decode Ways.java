0a71007d6b4f5b7db7c8f21121166e958bbf5021
HannibalCJH <hannibalcjh@gmail.com>
2016-08-11 20:21:30 -0500

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/91. Decode Ways.java
+++ 91-Decode-Ways/91. Decode Ways.java
@@ -10,8 +10,10 @@ public class Solution {
         dp[1] = 1;
         for(int i = 2; i < s.length() + 1; i++)
         {
+            // 取前两个字符看能不能解码
             if(Integer.parseInt(s.substring(i - 2, i)) >= 10 && Integer.parseInt(s.substring(i - 2, i)) <= 26)
                 dp[i] += dp[i - 2];
+            // 取上一个字符看能不能解码
             if(Integer.parseInt(s.substring(i - 1, i)) > 0)
                 dp[i] += dp[i - 1];
         }
