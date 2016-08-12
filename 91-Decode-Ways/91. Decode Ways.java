2619aafee975219790e662ed6a990c4662d5c20a
HannibalCJH <hannibalcjh@gmail.com>
2016-08-11 20:19:42 -0500

    [91. Decode Ways][Accepted]committed by LeetCode Extension

--- 91-Decode-Ways/91. Decode Ways.java
+++ 91-Decode-Ways/91. Decode Ways.java
@@ -1,24 +1,18 @@
 public class Solution {
     public int numDecodings(String s) 
-	{
-        if(s.length() == 0) 
-        	return s.length();
-        
-        // 如果第一位是0，则无法解码
-        if(s.charAt(0) == '0')
+    {
+        // 字符串为空或者首字符是0都无法解码
+        if(s.length() == 0 || s.charAt(0) == '0')
             return 0;
             
         int[] dp = new int[s.length() + 1];
-        // 初始化第一种解码方式
-        dp[0] = 1;
+        dp[0] = 1; 
         dp[1] = 1;
-        for(int i=2;i<=s.length();i++)
+        for(int i = 2; i < s.length() + 1; i++)
         {
-            // 如果字符串的第i-1位和第i位能组成一个10到26的数字，说明我们可以在第i-2位的解码方法上继续解码
-            if(Integer.parseInt(s.substring(i - 2, i)) <= 26 && Integer.parseInt(s.substring(i - 2, i)) >= 10)
+            if(Integer.parseInt(s.substring(i - 2, i)) >= 10 && Integer.parseInt(s.substring(i - 2, i)) <= 26)
                 dp[i] += dp[i - 2];
-            // 如果字符串的第i-1位和第i位不能组成有效二位数字，在第i-1位的解码方法上继续解码
-            if(Integer.parseInt(s.substring(i - 1, i)) != 0)
+            if(Integer.parseInt(s.substring(i - 1, i)) > 0)
                 dp[i] += dp[i - 1];
         }
         return dp[s.length()];
