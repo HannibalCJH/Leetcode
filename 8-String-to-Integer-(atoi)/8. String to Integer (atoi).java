afaa59d6cdfc3c35cca180374d0f8cca1f4cbcfa
HannibalCJH <hannibalcjh@gmail.com>
2016-08-12 21:25:05 -0500

    [8. String to Integer (atoi)][Accepted]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/8. String to Integer (atoi).java
+++ 8-String-to-Integer-(atoi)/8. String to Integer (atoi).java
@@ -1,47 +1,41 @@
 public class Solution {
     public int myAtoi(String str) 
-	{
-		if (str == null || str.length() < 1)
-			return 0;
-	 
-		// 剪除字符串两边的空格
-		str = str.trim();
-	 
-		boolean minus = false;
-	 
-		// 检查正负
-		int i = 0;
-		if(str.charAt(0) == '-') 
-		{
-			minus = true;
-			i++;
-		} 
-		else if(str.charAt(0) == '+')
-			i++;
-		// 用double型来存结果
-		double result = 0;
-	 
-		// 计算数字结果
-		
-		for(;i<str.length();i++)
-		{
-			char cur = str.charAt(i);
-			if(cur >= '0' && cur <= '9')
-				result = result * 10 + (str.charAt(i) - '0');
-			else
-				break;
-		}
-	 
-		if(minus)
-			result = -result;
-	 
-		// 检查是否越界
-		if(result > Integer.MAX_VALUE)
-			return Integer.MAX_VALUE;
-	 
-		if(result < Integer.MIN_VALUE)
-			return Integer.MIN_VALUE;
-	 
-		return (int)result;
+    {
+        // 剪除两边的空格
+        str = str.trim();
+        if(str.length() == 0)
+            return 0;
+            
+        boolean minus = false;
+        int index = 0;
+        
+        // 判断正负
+        if(str.charAt(index) == '-')
+        {
+            minus = true;
+            index++;
+        }
+        else if(str.charAt(index) == '+')
+            index++;
+        
+        // 用double型变量来存
+        double result = 0;
+        for(; index < str.length(); index++)
+        {
+            char cur = str.charAt(index);
+            if(cur >= '0' && cur <= '9')
+                result = result * 10 + (cur - '0');
+            else
+                break;
+        }
+        
+        if(minus)
+            result = -result;
+        
+        if(result > Integer.MAX_VALUE)
+            return Integer.MAX_VALUE;
+        if(result < Integer.MIN_VALUE)
+            return Integer.MIN_VALUE;
+        return (int)result;
     }
 }
