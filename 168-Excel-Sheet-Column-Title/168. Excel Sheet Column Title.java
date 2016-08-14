d17d2f73c84bc58fc34f7765d253eb476dd33e43
HannibalCJH <hannibalcjh@gmail.com>
2016-08-14 14:17:14 -0500

    [168. Excel Sheet Column Title][Accepted]committed by LeetCode Extension

--- 168-Excel-Sheet-Column-Title/168. Excel Sheet Column Title.java
+++ 168-Excel-Sheet-Column-Title/168. Excel Sheet Column Title.java
@@ -1,21 +1,20 @@
 public class Solution {
-    // 我的算法
-	public String convertToTitle(int n) 
-	{
+    public String convertToTitle(int n) 
+    {
         StringBuilder sb = new StringBuilder();
-        int remainder;
         while(n > 0)
         {
-        	remainder = n % 26;  // 1~25为"A"~"Y"
-        	n /= 26;
-        	// 0其实是"Z"
-        	if(remainder == 0)
-        	{
-        		remainder = 26;  // 重设26为"Z"
-        		n--;             // 在本来余数是0的情况下会向下一位进1，因为这个时候其实是"Z"而没有进位所以把这个1减回来
-        	}
-        	sb.append((char)('A' - 1 + remainder));
+            // 字母是A~Y的情况
+            int remainder = n % 26;
+            n /= 26;
+            // 字母是Z的情况
+            if(remainder == 0)
+            {
+                remainder = 26;
+                n--;
+            }
+            sb.insert(0, (char)(remainder + 'A' - 1));
         }
-        return sb.reverse().toString();
-    } 
+        return sb.toString();
+    }
 }
