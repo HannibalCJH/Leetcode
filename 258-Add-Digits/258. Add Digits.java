05d029feee95aadcca6615ea50b195be011a89bc
HannibalCJH <hannibalcjh@gmail.com>
2016-08-15 18:54:31 -0500

    [258. Add Digits][Accepted]committed by LeetCode Extension

--- 258-Add-Digits/258. Add Digits.java
+++ 258-Add-Digits/258. Add Digits.java
@@ -1,18 +1,16 @@
 public class Solution {
 	public int addDigits(int num) 
 	{
-		int temp = num, sum = 0;
-		while(temp >= 10)
-		{
-			sum = 0;
-			while(temp > 0)
-			{
-				int remainder = temp % 10;
-				temp /= 10;
-				sum += remainder;
-			}
-			temp = sum;
-		}
-		return temp;
+        while(num > 9)
+        {
+            int temp = num, sum = 0;
+            while(temp > 0)
+            {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            num = sum;
+        }
+        return num;
     }
 }
