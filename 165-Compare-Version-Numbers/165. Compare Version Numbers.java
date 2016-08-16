1cedab8174fc0ee0ba6dde2a6d9ca6a2dd75df8a
HannibalCJH <hannibalcjh@gmail.com>
2016-08-15 19:37:38 -0500

    [165. Compare Version Numbers][Accepted]committed by LeetCode Extension

--- 165-Compare-Version-Numbers/165. Compare Version Numbers.java
+++ 165-Compare-Version-Numbers/165. Compare Version Numbers.java
@@ -1,42 +1,33 @@
 public class Solution {
     public int compareVersion(String version1, String version2) 
-	{
-        // 按照.进行分割
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        int i;
-        // 比对相应的子串
-        for(i=0;i<v1.length && i<v2.length;i++)
+    {
+        String[] ver1 = version1.split("\\.");
+        String[] ver2 = version2.split("\\.");
+        
+        int index = 0;
+        while(index < ver1.length && index < ver2.length)
         {
-            int val1 = Integer.parseInt(v1[i]);
-            int val2 = Integer.parseInt(v2[i]);
-            if(val1 > val2) 
-            	return 1;
-            if(val1 < val2) 
-            	return -1;
+            int v1 = Integer.parseInt(ver1[index]);
+            int v2 = Integer.parseInt(ver2[index]);
+            
+            if(v1 > v2) 
+                return 1;
+            if(v1 < v2) 
+                return -1;
+            index++;
         }
         
-        // 如果某个版本号更长，判断其多余部分是否是0，如果不是0，则较长的较大，否则是一样的
-        if(i < v1.length)
+        while(index < ver1.length)
         {
-            for(;i<v1.length;i++)
-            {
-                int val = Integer.parseInt(v1[i]);
-                if(val != 0) 
-                	return 1;
-            }
+            if(Integer.parseInt(ver1[index++]) > 0)
+                return 1;
         }
         
-        if(i < v2.length)
+        while(index < ver2.length)
         {
-            for(;i<v2.length;i++)
-            {
-                int val = Integer.parseInt(v2[i]);
-                if(val != 0) 
-                	return -1;
-            }
-        } 
-        
+            if(Integer.parseInt(ver2[index++]) > 0)
+                return -1;
+        }
         return 0;
     }
 }
