9d0b8dc681566f0f764c5fc21da659ac16ddca50
HannibalCJH <hannibalcjh@gmail.com>
2016-08-15 19:39:33 -0500

    [165. Compare Version Numbers][Accepted]committed by LeetCode Extension

--- 165-Compare-Version-Numbers/165. Compare Version Numbers.java
+++ 165-Compare-Version-Numbers/165. Compare Version Numbers.java
@@ -1,10 +1,13 @@
 public class Solution {
+    // 我的算法
     public int compareVersion(String version1, String version2) 
     {
+        // 按照.进行分割
         String[] ver1 = version1.split("\\.");
         String[] ver2 = version2.split("\\.");
         
         int index = 0;
+        // 比较对相应的子串
         while(index < ver1.length && index < ver2.length)
         {
             int v1 = Integer.parseInt(ver1[index]);
@@ -17,6 +20,7 @@ public class Solution {
             index++;
         }
         
+        // 如果某个版本号更长，判断其多余部分是否是0，如果不是0，则较长的较大，否则是一样的
         while(index < ver1.length)
         {
             if(Integer.parseInt(ver1[index++]) > 0)
