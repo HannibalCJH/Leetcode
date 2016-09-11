4807124961a7760830ed6e20dd6c6e8802ae5518
HannibalCJH <hannibalcjh@gmail.com>
2016-09-11 14:38:43 -0500

    [396. Rotate Function][Accepted]committed by LeetCode Extension

---
+++ 396-Rotate-Function/396. Rotate Function.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int maxRotateFunction(int[] A) 
+    {
+        if(A == null || A.length == 0)
+            return 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < A.length; i++)
+            max = Math.max(rotateFunction(A, i), max);
+        return max;
+    }
+    
+    private int rotateFunction(int[] A, int startPos)
+    {
+        int factor = 1, sum = 0, index = startPos + 1;
+        int n = A.length;
+        if(index >= n)
+        	index = 0;
+        
+        while(index != startPos)
+        {
+            sum += factor * A[index++];
+            factor++;
+            if(index >= n)
+                index = 0;
+        }
+        return sum;
+    }
+}
