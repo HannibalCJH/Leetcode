dafade135616d50de8cb1608dfc05d3dc2bb377b
HannibalCJH <hannibalcjh@gmail.com>
2016-09-13 16:30:30 -0500

    [242. Valid Anagram][Accepted]committed by LeetCode Extension

--- 242-Valid-Anagram/242. Valid Anagram.java
+++ 242-Valid-Anagram/242. Valid Anagram.java
@@ -1,12 +1,18 @@
 public class Solution {
+    // 我的算法
     public boolean isAnagram(String s, String t) 
 	{
-		char[] charAry1 = s.toCharArray();
-		char[] charAry2 = t.toCharArray();
-		
-		Arrays.sort(charAry1);
-		Arrays.sort(charAry2);
-		
-		return (String.valueOf(charAry1).equals(String.valueOf(charAry2)));
-    }
+	    if(s.length() != t.length())
+	        return false;
+        int[] bitmap = new int[26];
+        for(int i = 0; i < s.length(); i++)
+        {
+            bitmap[s.charAt(i) - 'a']++;
+            bitmap[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0; i < bitmap.length; i++)
+            if(bitmap[i] > 0)
+                return false;
+        return true;
+	}
 }
