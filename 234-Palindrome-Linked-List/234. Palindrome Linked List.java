4663fdd96132cfb0f7f1f5ab828faa8fe02fca75
HannibalCJH <hannibalcjh@gmail.com>
2016-09-14 16:54:55 -0500

    [234. Palindrome Linked List][Accepted]committed by LeetCode Extension

--- 234-Palindrome-Linked-List/234. Palindrome Linked List.java
+++ 234-Palindrome-Linked-List/234. Palindrome Linked List.java
@@ -23,7 +23,7 @@ public class Solution {
         int[] half = new int[count / 2];  // 存储前一半节点值，奇数个情况下不包括中间点
         // 以下两个for循环都为遍历第二遍，分别是前半部分和后半部分
         // 第二遍遍历前半部分，存储前一半的值
-        for(int i=0;i<count/2;i++)
+        for(int i = 0; i < count / 2;i++)
         {
         	half[i] = temp.val;
         	temp = temp.next;
@@ -32,7 +32,7 @@ public class Solution {
         if(count % 2 > 0)
         	temp = temp.next;
         // 第二遍遍历后半部分，逐一比较
-        for(int i=count/2-1;i>=0;i--)
+        for(int i = count / 2 - 1; i >= 0; i--)
         {
         	if(temp.val != half[i])
         		return false;
