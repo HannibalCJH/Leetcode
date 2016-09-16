49e3596e2fab95cce9edb2b5be42585678177694
HannibalCJH <hannibalcjh@gmail.com>
2016-09-15 21:38:32 -0500

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/200. Number of Islands.java
+++ 200-Number-of-Islands/200. Number of Islands.java
@@ -1,30 +1,34 @@
 public class Solution {
+    // 我的算法
     public int numIslands(char[][] grid) 
     {
         if(grid.length == 0 || grid[0].length == 0)
             return 0;
-        int rows = grid.length, cols = grid[0].length;
+        
+        int m = grid.length, n = grid[0].length;
         int count = 0;
-        for(int i = 0; i < rows; i++)
-            for(int j = 0; j < cols; j++)
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
                 if(grid[i][j] == '1')
                 {
-                    dfs(i, j, grid);
+                    dfs(grid, i, j);
                     count++;
                 }
         return count;
     }
     
-    private void dfs(int row, int col, char[][] grid)
+    // 深度优先搜索
+    private void dfs(char[][] grid, int x, int y)
     {
-        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0')
+        // 返回条件判断
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == '0')
             return;
-        // 把访问过的变成0
-        grid[row][col] = '0';
-        
-        dfs(row + 1, col, grid);
-        dfs(row - 1, col, grid);
-        dfs(row, col + 1, grid);
-        dfs(row, col - 1, grid);
+        // 把已访问的元素设为0
+        grid[x][y] = '0';
+        // 朝四个方向深度优先搜索
+        dfs(grid, x - 1, y);
+        dfs(grid, x + 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
     }
 }
