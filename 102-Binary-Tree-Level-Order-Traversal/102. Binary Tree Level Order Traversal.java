2defcafaaecba745241c9ca1e1e004476a7cadd5
HannibalCJH <hannibalcjh@gmail.com>
2016-09-17 10:45:06 -0500

    [102. Binary Tree Level Order Traversal][Accepted]committed by LeetCode Extension

--- 102-Binary-Tree-Level-Order-Traversal/102. Binary Tree Level Order Traversal.java
+++ 102-Binary-Tree-Level-Order-Traversal/102. Binary Tree Level Order Traversal.java
@@ -8,6 +8,7 @@
  * }
  */
 public class Solution {
+    // 我的算法
     public List<List<Integer>> levelOrder(TreeNode root) 
     {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
@@ -18,17 +19,22 @@ public class Solution {
         queue.offer(root);
         while(!queue.isEmpty())
         {
+            // 这一层的节点数
             int size = queue.size();
+            // 记录这一层的节点值
             List<Integer> curLevel = new ArrayList<Integer>();
             for(int i = 0; i < size; i++)
             {
                 TreeNode node = queue.poll();
+                // 加入节点值
                 curLevel.add(node.val);
+                // 左右子树不会空则加入队列
                 if(node.left != null)
                     queue.offer(node.left);
                 if(node.right != null)
                     queue.offer(node.right);
             }
+            // 把这层的节点值加入到结果集中
             result.add(curLevel);
         }
         return result;
