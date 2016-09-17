2f895039d4de4a2368d31c0e62634f4974c2ecaf
HannibalCJH <hannibalcjh@gmail.com>
2016-09-17 15:05:24 -0500

    [17. Letter Combinations of a Phone Number][Accepted]committed by LeetCode Extension

--- 17-Letter-Combinations-of-a-Phone-Number/17. Letter Combinations of a Phone Number.java
+++ 17-Letter-Combinations-of-a-Phone-Number/17. Letter Combinations of a Phone Number.java
@@ -1,26 +1,27 @@
 public class Solution {
-    public List<String> letterCombinations(String digits) 
-    {
-        LinkedList<String> ans = new LinkedList<String>();
-        
-        if(digits == null || digits.length() < 1)
-            return ans;
-        
-        // 数字字母对应表
-        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        ans.add("");
-        
-        for(int i = 0; i < digits.length(); i++)
-        {
-            //int x = Character.getNumericValue(digits.charAt(i));
-            int digit = digits.charAt(i) - '0';
-            while(ans.peek().length() == i)
-            {
-                String t = ans.remove();
-                for(char s : mapping[digit].toCharArray())
-                    ans.add(t + s);
-            }
-        }
-        return ans;
-    }
+    private static final String[] KEYS = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    
+	public List<String> letterCombinations(String digits) 
+	{
+		List<String> result = new ArrayList<String>();
+		if(digits == null || digits.length() == 0)
+		    return result;
+		combination("", digits, 0, result);
+		return result;
+	}
+
+	private void combination(String prefix, String digits, int offset, List<String> result) 
+	{
+		// 得到一个规定长度的组合
+		if(offset >= digits.length()) 
+		{
+			result.add(prefix);
+			return;
+		}
+		// 获取这个数字可以表示的所有字母
+		String letters = KEYS[(digits.charAt(offset) - '0')];
+		// 对每一个可能的字母递归并回溯
+		for(int i = 0; i < letters.length(); i++)
+			combination(prefix + letters.charAt(i), digits, offset + 1, result);
+	}
 }
