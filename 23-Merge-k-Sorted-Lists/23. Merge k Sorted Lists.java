33a63284c40e1b0bdad3708b924953f487e257ad
HannibalCJH <hannibalcjh@gmail.com>
2016-09-17 14:09:49 -0500

    [23. Merge k Sorted Lists][Accepted]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/23. Merge k Sorted Lists.java
+++ 23-Merge-k-Sorted-Lists/23. Merge k Sorted Lists.java
@@ -10,7 +10,8 @@ public class Solution {
     public ListNode mergeKLists(ListNode[] lists) 
     {
         if(lists == null || lists.length == 0)  
-            return null;  
+            return null;
+        // 比较器
         Comparator<ListNode> comparator = new Comparator<ListNode>()
         {  
             public int compare(ListNode m, ListNode n)
@@ -26,16 +27,16 @@ public class Solution {
         
         PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.length, comparator);
         
+        // 把所有链表加入优先队列，并按照链表头节点的大小排序
         for(int i = 0; i < lists.length; i++)
-        {  
             if(lists[i] != null)  
             	queue.add(lists[i]);  
-        }
         
         ListNode head = null, current = null;
         
         while(!queue.isEmpty())
-        {  
+        { 
+        	// 每次从优先队列中得到值最小的节点
             if(head == null)
             {  
                 head = queue.poll();  
@@ -45,7 +46,8 @@ public class Solution {
             {  
             	current.next = queue.poll();  
             	current = current.next;  
-            }  
+            }
+            // 这个被选取的链表还有剩余的节点则把下一个节点加入优先队列并排序
             if(current.next != null)  
             	queue.add(current.next);  
         }  
